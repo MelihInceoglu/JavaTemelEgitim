7f6a5b629f10a4f9d37079e50e9e3bca2059233b
MelihInceoglu <av.melihinceoglu@gmail.com>
2023-04-27 12:08:35 +0300

    4.gun commit

--- src/day42_Collections/C02_Deque.java
+++ src/day42_Collections/C02_Deque.java
@@ -6,28 +6,42 @@ import java.util.LinkedList;
 public class C02_Deque {
     public static void main(String[] args) {
 
+
         // Deque Double ended queue
         //çift taraflı kuyruk
 
 
         Deque<String> harfler=new LinkedList<>();
+
         harfler.add("J");
+
         harfler.add("K");
+
         System.out.println(harfler);//[J, K]
 
+
         harfler.addFirst("B"); //[B, J, K]
+
         System.out.println(harfler);
 
         System.out.println(harfler.remove()); // B
+
         System.out.println(harfler);
 
         harfler.addFirst("T"); // T,J,K
+
         harfler.addLast("T");//T,J,K,T
+
         harfler.add("L");//T,J,K,T,L
+
         System.out.println(harfler);
+
         System.out.println(harfler.removeLastOccurrence("T"));//true
+
         System.out.println(harfler);//[T, J, K, L]
+
         System.out.println(harfler.removeFirstOccurrence("R"));//false
+
         System.out.println(harfler);
 
         System.out.println(harfler.peekFirst());
